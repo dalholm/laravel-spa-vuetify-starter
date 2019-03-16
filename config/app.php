bd86489b94414c2358c7feaceaa77cf4b86c048f
Abel Saul Miraval Gomez <abelmiraval@hotmail.com>
2019-03-16 13:07:45 -0500

    Update .env

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'Europe/Stockholm'),
+    'timezone' => env('APP_TIMEZONE', 'America/Lima'),
 
     /*
     |--------------------------------------------------------------------------
